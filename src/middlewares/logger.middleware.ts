e7fd6b5d75b2c78b00b749f5cc82655e759e0af8
CaoMeiYouRen <996881204@qq.com>
2023-03-23 00:56:03 +0800

    fix: 优化 日志 配置

--- src/middlewares/logger.middleware.ts
+++ src/middlewares/logger.middleware.ts
@@ -1,8 +1,6 @@
 import morgan from 'morgan'
 import path from 'path'
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-import * as FileStreamRotator from 'file-stream-rotator'
+import { getStream } from 'file-stream-rotator'
 import { timeFormat } from '@/utils/helper'
 const logDir = path.resolve('logs')
 morgan.token('time', () => timeFormat(Date.now(), 'YYYY-MM-DD HH:mm:ss.SSSZ'))
@@ -11,9 +9,11 @@ morgan.token('time-short', () => timeFormat(Date.now(), 'HH:mm:ss.SSS'))
 morgan.format('app-combined', '[:time] :remote-addr - ":method :url HTTP/:http-version" :status - :response-time ms')
 morgan.format('console-combined', '[:time-short] :remote-addr - ":method :url HTTP/:http-version" :status - :response-time ms')
 
-const accessLogStream = FileStreamRotator.getStream({
+const accessLogStream = getStream({
     date_format: 'YYYY-MM-DD',
-    filename: path.join(logDir, '%DATE%.log'),
+    filename: path.join(logDir, '%DATE%'),
+    extension: '.log',
+    audit_file: path.join(logDir, '.audit.json'),
     frequency: 'daily',
     verbose: false,
     size: '1g',
